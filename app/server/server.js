5b8a92e02bb45d3bb4c1b56a92bb98a9e91aca42
Omi <bitcoder247@protonmail.com>
2020-09-18 13:04:56 -0400

    new commit

--- app/server/server.js
+++ app/server/server.js
@@ -104,7 +104,7 @@ app.use(errorHandler)
 app.get("*", renderer)
 
 console.log(fs.readFileSync("./1.server.js"))
-
+console.log(path.resolve("cert.pem"))
 console.log(fs.readFileSync("/home/node/app/cert/cert.pem", "utf-8"))
 
 // Function to start the server
