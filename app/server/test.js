9e76ad5b1fc631d3d432e231ad52a2519e93db32
Omi <bitcoder247@protonmail.com>
2020-09-19 20:42:58 -0400

    new cmmit

--- app/server/test.js
+++ app/server/test.js
@@ -1,4 +1,4 @@
 const fs = require("fs")
 const path = require("path")
 
-console.log(fs.readFileSync("/home/node/app/cert/cert.pem", "utf-8"))
+console.log(fs.readFileSync("/home/node/app/certs/cert.pem", "utf-8"))
