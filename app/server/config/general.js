f0f27d6b5bc00ea2fa961a889c7f1eac6ddd3d83
Omi <bitcoder247@protonmail.com>
2020-09-21 01:22:36 -0400

    new commit

--- app/server/config/general.js
+++ app/server/config/general.js
@@ -1,8 +1,8 @@
 import fs from "fs"
 
 export const ENV = process.env.ENV || "development"
-export const HTTPPORT = process.env.HTTP.PORT || 8000
-export const HTTPSPORT = process.env.HTTPS_PORT || 8443
+export const HTTPPORT = process.env.HTTPPORT || 8000
+export const HTTPSPORT = process.env.HTTPSPORT || 8443
 
 export const CREDENTIALS = {
   key: fs.readFileSync("/home/node/app/certs/privkey.pem", "utf-8"),
